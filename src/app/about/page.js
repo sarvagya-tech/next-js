dd3c115d06e2f348f477c0ba80bdd3f049b6133a
sarvagya-tech <sarvagyam93@gmail.com>
2026-07-21 23:33:59 +0530

    Add about and contact pages with basic content

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>this is the about page !!</div>
+  )
+}
+
+export default about;
---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function contact() {
+  return (
+    <div>this is my contact page </div>
+  )
+}
+
+export default contact
